c21a0fddf14bddda5d1a4b33ae7336dc7a8b1acc
mejat9806 <110531287+mejat9806@users.noreply.github.com>
2024-03-07 17:19:26 +0800

    again

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,22 +1,34 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Access the MongoDB URI from the environment variables
 const mongoURL = process.env.MONGO;
+
+// Object to track connection status
 const connection: any = {};
 
+// Function to connect to MongoDB
 export const connectToDb = async () => {
   try {
+    // Check if the connection already exists
     if (connection.isConnected) {
       console.log("Connection exists");
       return;
     }
 
+    // Connect to MongoDB using the URI from environment variables
     const db = await mongoose.connect(mongoURL as string);
+
+    // Set the connection status
     connection.isConnected = db.connections[0].readyState;
+
+    // Log successful connection
     console.log("Connected to MongoDB");
   } catch (error) {
+    // Handle connection error
     console.error("Error connecting to MongoDB:", error);
     throw new Error("DB error");
   }
