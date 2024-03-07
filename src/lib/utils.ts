f837f71f74f566475cdf25fab316313ccf9e279e
mejat9806 <110531287+mejat9806@users.noreply.github.com>
2024-03-07 17:14:09 +0800

    env time

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,16 +12,14 @@ export const connectToDb = async () => {
       return;
     }
 
-    /*  const mongoURL = process.env.MONGO;
+    const mongoURL = process.env.MONGO;
     console.log("MongoDB URI:", mongoURL); // Add this line for debugging
     if (!mongoURL) {
       throw new Error("MongoDB URI not found in environment variables");
     }
 
-    console.log("Connecting to MongoDB:", mongoURL); */
-    const db = await mongoose.connect(
-      "mongodb+srv://ameraizat98:ameraizat98@cluster0.ojb3uvl.mongodb.net/next14tuto?retryWrites=true&w=majority&appName=Cluster0",
-    );
+    console.log("Connecting to MongoDB:", mongoURL);
+    const db = await mongoose.connect(mongoURL);
     connection.isConnected = db.connections[0].readyState;
     console.log("Connected to MongoDB");
   } catch (error) {
